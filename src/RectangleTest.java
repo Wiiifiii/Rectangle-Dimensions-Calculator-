842f9830fb2aecc1ae779713ee6fe88d96a3b693
Wefky Hamed <97392010+Wiiifiii@users.noreply.github.com>
2024-09-14 07:42:41 +0300

    Add RectangleTest class to test Rectangle functionality
    - Implemented RectangleTest class with a main method.
    - Created a Rectangle object to test user input for width and height.
    - Tested area calculation and printed rectangle details (width, height, and area).

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -1,6 +1,6 @@
 public class RectangleTest {
     public static void main(String[] args) {
-        // Create an instance of the Rectangle class to call the class's methods
+        // Create an instance of the Rectangle class to call the class's methods (new object).
         Rectangle rectangle = new Rectangle();
         
         // Test the input method to ask width and height
